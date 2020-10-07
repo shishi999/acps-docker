8028c29836bee28c2f8ce1aabac0db55001d61dc
Ubuntu <ricksoft@testDockerhost.wciglgfbgnoezmdn0k01hxgwhg.xx.internal.cloudapp.net>
2020-10-07 07:13:44 +0000

    Add ACS

--- db/init/create-user-and-db.sql
+++ db/init/create-user-and-db.sql
@@ -1,5 +1,7 @@
 CREATE ROLE alfresco LOGIN PASSWORD 'alfresco';
 create database activiti;
 create database activitiadm;
+create database alfresco;
 grant all privileges on database activiti to alfresco;
 grant all privileges on database activitiadm to alfresco;
+grant all privileges on database alfresco to alfresco;
